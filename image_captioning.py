d2f6eb0ab60f990c70140a6f1d51f4199018f703
Nageshmanidatta L V <nagesh3lv@gmail.com>
2024-10-19 18:05:08 +0530

    Update image_captioning.py

--- image_captioning.py
+++ image_captioning.py
@@ -2,7 +2,7 @@ import transformers
 from PIL import Image
 import requests
 import gradio as gr
-from transformers import BlipProcessor, BlipForConditionalGeneration
+from transformers import AutoProcessor, BlipForConditionalGeneration
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
 def img_captioning(image):
